012fd850c7d7688248df6eb9b867fde90a162a8c
Brady <brady800526@gmail.com>
2018-01-25 23:06:46 +0800

    fix episode4 type

--- deep-net.py
+++ deep-net.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-from tensorflow.examples.tutorials.minist import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 
-minist = input_data.read_data_sets("/tmp/data", one_hot=True)
+mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
 
 n_nodes_1 = 500
 n_nodes_2 = 500
@@ -49,28 +49,31 @@ def neural_network_model(data):
 
 	output = tf.matmul(l3, output_layer['weights']) + output_layer['bias']
 
+	return output
+
 
 def train_neural_network(x):
 	prediction = neural_network_model(x)
-	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction, y))
-	optimizer = rf.train.AdamOptimizer().minimize(cost)
+	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+	optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 	hm_epochs = 10
 
 	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
 
 		for epoch in range(hm_epochs):
 			epoch_loss = 0
 
-			for _ in range(int(minist.train.num_examples/batch_size)):
-				x, y = minist.train.next_batch(batch_size)
-				_, c = sess.run([optimzier, cost], feed_dict = {x:x, y:y})
+			for _ in range(int(mnist.train.num_examples/batch_size)):
+				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+				_, c = sess.run([optimizer, cost], feed_dict = {x:epoch_x, y:epoch_y})
 				epoch_loss += c
 			print('Epoch', epoch, 'complete out of', hm_epochs, 'loss:', epoch_loss)
 
 		correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-		print("Accuracy", accuracy.eval({x:minist.test.images, y:minist.test.labels}))
+		print("Accuracy", accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
 
 train_neural_network(x)
 
